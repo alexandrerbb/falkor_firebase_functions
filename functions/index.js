e4f0c46a14fcd1a801bf3bcd661587b8763ac4c4
alexandrerbb <82387915+alexandrerbb@users.noreply.github.com>
2023-05-14 14:03:27 +0200

    modified:   functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -9,20 +9,32 @@ exports.commandHistory = functions.https.onCall((data, context) => {
 
 });
 
-exports.orderBasket = functions.https.onCall((data, context) => {
+
+const returns = {
+    ok: {
+        status_code: 0,
+        message: "votre commande a été passée avec succès."
+    },
+}
+
+
+exports.orderBasket = functions.https.onCall(async (data, context) => {
 
     if (!context.auth) return "Please log in"
     const userData = context.auth.token
 
     const firestore = admin.firestore();
 
+    // Vérifie que le champs zone est bien passé en en paramètres.
     if (!data.hasOwnProperty('zone'))
         throw new functions.https.HttpsError(
             "internal",
             "Zone n'est pas renseigné",
             error
         );
-    firestore.collection("delivery_sites").where('name', '==', data?.zone).get()
+
+    // Vérifie que le site de livraison existe.
+    await firestore.collection("delivery_sites").where('name', '==', data?.zone).get()
         .then(querySnapshot => {
             if (querySnapshot.empty) {
                 throw new functions.https.HttpsError(
@@ -36,14 +48,37 @@ exports.orderBasket = functions.https.onCall((data, context) => {
             console.error('Erreur lors de la vérification de la collection delivery_sites :', error);
         });
 
+    // Vérifie que les produits sont toujours en stock.
     for (let product in data?.basket) {
-        const basketQuantity = data?.basket[product]?.quantity
-        if (Number.isInteger(basketQuantity)) {
+        // Product est la clé du produit et est équivalent au champs 'name' dans la base firestore.
+        const basketQuantity = data?.basket[product]?.quantity // On récupère la quantité du produit.
 
-        }
+        if (Number.isInteger(basketQuantity)) { // On vérifie que la quantité récupérée est typée de manière cohérente (et non nulle.)
 
+            await firestore.collection("products").where('name', '==', product).get().then(querySnapshot => {
+                // On va vérifier que les stocks sont suffiants.
+                if (querySnapshot.empty) throw new functions.https.HttpsError(
+                    "internal",
+                    "Produit n'existe pas en base.",
+                    error
+                )
+                else {
+
+                }
+
+            }).catch(error => {
+                console.error('Erreur lors de la récupération des produits dans la base de données :', error)
+            })
+        } else
+            throw new functions.https.HttpsError(
+                "internal",
+                "Problème lors de la vérification des stocks",
+                error
+            );
     }
 
+
+    // Data envoyée dans 'orders' finalement.
     order = {
         basket: { ...data.basket },
         'name': userData?.name,
@@ -56,8 +91,8 @@ exports.orderBasket = functions.https.onCall((data, context) => {
 
     console.log(order)
 
-    return
-    return firestore.collection("orders").add(data)
+    // Envoie des données en base.
+    firestore.collection("orders").add(data)
         .then((docRef) => {
             return {
                 message: `Commande créée.`
