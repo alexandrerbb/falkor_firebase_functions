995e164eec9ee45d31bf85fcbd059fd9d15a0cc0
alexandrerbb <82387915+alexandrerbb@users.noreply.github.com>
2023-05-14 13:10:09 +0200

    confirm command

--- functions/index.js
+++ functions/index.js
@@ -6,5 +6,21 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.orderBasket = functions.https.onCall((data, context) => {
+    if (!context.auth) return "Please log in"
 
+    const firestore = admin.firestore();
+
+    return firestore.collection("orders").add(data)
+        .then((docRef) => {
+            return {
+                message: `Commande créée.`
+            };
+        })
+        .catch((error) => {
+            throw new functions.https.HttpsError(
+                "internal",
+                "Une erreur s'est produite lors de l'ajout de l'utilisateur.",
+                error
+            );
+        });
 })
